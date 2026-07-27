c12ae7fdf3aa1e95af3eb6e19ad1f4c5ba40f2ed
Cloud-Strife707 <ishathishitha123@gmail.com>
2026-07-27 18:55:03 +0530

    error fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,30 +4,33 @@ import AirQualityCard from "../components/AirQualityCard";
 import TemperatureChart from "../components/TemperatureChart";
 import FavoriteCities from "../components/FavoriteCities";
 import ForecastCard from "../components/ForecastCard";
-import TodaysHighlights from "../components/Today'sHighlights";
+import TodaysHighlights from "../components/TodaysHighlights";
 import WeatherRadar from "../components/WeatherRadar";
-import { useEffect } from "react";
 import { getCurrentWeather } from "../services/weatherService";
 import { useEffect, useState } from "react";
 
 
 function Home() {
-  
-  useEffect(() => {
-  async function loadWeather() {
-    try {
-      const [weather, setWeather] = useState(null);
-      const data = await getCurrentWeather("Colombo");
-      
-setWeather(data);
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
-  loadWeather();
-}, []);
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    async function loadWeather() {
+      try {
+        const data = await getCurrentWeather("Colombo");
+
+        setWeather(data);
+        console.log(data);
+
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    loadWeather();
+  }, []);
+
+
   return (
     <div className="min-h-screen bg-[#081220] text-white">
 
@@ -38,7 +41,7 @@ setWeather(data);
         <div className="grid grid-cols-12 gap-6">
 
           <div className="col-span-8">
-            <HeroCard />
+            <HeroCard weather={weather} />
           </div>
 
           <div className="col-span-4">
